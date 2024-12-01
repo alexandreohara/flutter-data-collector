45ed3744ddf9f4067c10b65f11fded24f3b6eeab
Alexandre <alexandre.ohara@hotmail.com>
2024-12-01 11:25:40 -0300

    create method to add fields to sheets

--- lib/service_account.dart
+++ lib/service_account.dart
@@ -1,19 +1,25 @@
 import 'dart:convert';
+import 'package:data_collector/models/Item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:googleapis/drive/v3.dart' as drive;
 import 'package:googleapis/drive/v3.dart';
 import 'package:googleapis/sheets/v4.dart' as sheets;
+import 'package:googleapis/sheets/v4.dart';
 import 'package:googleapis_auth/auth_io.dart';
 
 class AuthService with ChangeNotifier {
   AuthClient? _client;
   drive.DriveApi? _driveApi;
   sheets.SheetsApi? _sheetsApi;
+  drive.File? _folder;
+  drive.File? _sheet;
 
   AuthClient? get client => _client;
   drive.DriveApi? get driveApi => _driveApi;
   sheets.SheetsApi? get sheetsApi => _sheetsApi;
+  drive.File? get folder => _folder;
+  drive.File? get sheet => _sheet;
 
   Future<void> authenticate() async {
     try {
@@ -34,7 +40,7 @@ class AuthService with ChangeNotifier {
     }
   }
 
-  Future<drive.File> createOrFetchFolder(String folderName) async {
+  Future<String> createOrFetchFolder(String folderName) async {
     final driveApi = await _authenticateDrive();
 
     final query =
@@ -47,9 +53,11 @@ class AuthService with ChangeNotifier {
     print(fileList.files!.first.parents);
 
     if (fileList.files != null && fileList.files!.isNotEmpty) {
-      return fileList.files!.first;
+      return fileList.files!.first.id ?? '';
     }
-    return createFolder(driveApi, folderName);
+    _folder = await createFolder(driveApi, folderName);
+    notifyListeners();
+    return _folder!.id ?? '';
   }
 
   Future<drive.File> createFolder(DriveApi driveApi, String folderName) async {
@@ -66,32 +74,55 @@ class AuthService with ChangeNotifier {
     return folder;
   }
 
-  Future<drive.File> createOrFetchSheets(
-      String folderId, String sheetName) async {
-    final driveApi = await _authenticateDrive();
+  Future<String> createOrFetchSheets(String folderId, String sheetName) async {
+    try {
+      final driveApi = await _authenticateDrive();
+      final sheetsApi = await _authenticateSheets();
 
-    final query =
-        "'$folderId' in parents and name = '$sheetName' and mimeType = 'application/vnd.google-apps.spreadsheet' and trashed = false";
+      final query =
+          "'$folderId' in parents and name = '$sheetName' and mimeType = 'application/vnd.google-apps.spreadsheet' and trashed = false";
 
-    final fileList = await driveApi.files.list(
-        q: query, spaces: 'drive', $fields: 'files(id, name)', pageSize: 1);
+      final fileList = await driveApi.files.list(
+          q: query, spaces: 'drive', $fields: 'files(id, name)', pageSize: 1);
 
-    if (fileList.files != null && fileList.files!.isNotEmpty) {
-      final existingFile = fileList.files!.first;
-      print(
-          'Sheet already exists: ${existingFile.name}, ID: ${existingFile.id}');
-      return existingFile;
+      if (fileList.files != null && fileList.files!.isNotEmpty) {
+        final existingFile = fileList.files!.first;
+        print(
+            'Sheet already exists: ${existingFile.name}, ID: ${existingFile.id}');
+        return existingFile.id!;
+      }
+
+      final sheetMetadata = drive.File()
+        ..name = sheetName
+        ..mimeType = 'application/vnd.google-apps.spreadsheet'
+        ..parents = [folderId];
+
+      _sheet = await driveApi.files
+          .create(sheetMetadata, $fields: 'id, name, mimeType, parents');
+      await addFields(sheetsApi, _sheet!.id!);
+      notifyListeners();
+
+      return _sheet!.id ?? '';
+    } catch (e) {
+      print('Error creating or fetching sheet: $e');
+      rethrow;
     }
+  }
 
-    final sheetMetadata = drive.File()
-      ..name = sheetName
-      ..mimeType = 'application/vnd.google-apps.spreadsheet'
-      ..parents = [folderId];
-
-    final newSpreadsheet = await driveApi.files
-        .create(sheetMetadata, $fields: 'id, name, mimeType, parents');
-
-    return newSpreadsheet;
+  Future<void> addFields(SheetsApi sheetsApi, String spreadsheetId) async {
+    try {
+      final fields = Item.getFields();
+      final request = sheets.ValueRange(values: [fields]);
+      await sheetsApi.spreadsheets.values.update(
+        request,
+        spreadsheetId,
+        'A1',
+        valueInputOption: 'USER_ENTERED',
+      );
+    } catch (e) {
+      print('Error adding fields to spreadsheet: $e');
+      rethrow;
+    }
   }
 
   Future<drive.DriveApi> _authenticateDrive() async {
